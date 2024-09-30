d3bdda9df6ae324d2757d70948e4547f7708c6fb
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2024-09-30 18:57:32 +0330

    connect firstStep of login page to api

---
+++ src/components/layout/Login/FirstStep.tsx
@@ -0,0 +1,34 @@
+import { Control, Controller } from "react-hook-form";
+import * as SolarIconSet from "solar-icon-set";
+
+export default function FirstStep(props: {
+    control: Control
+}) {
+    return (
+        <div className="flex flex-col gap-y-[40px]">
+            <div className="flex flex-col gap-y-[16px]">
+                <h1 className="font-['IRANSansX'] text-[32px] font-bold">
+                    شماره موبایل
+                </h1>
+                <p className="text-sm font-medium font-['IRANSansXFaNum'] leading-tight text-[#64748B]">
+                    ما با اطمینان از واقعی بودن همه افرادی که در باکی هستند از کاربران خود محافظت می کنیم.
+                </p>
+            </div>
+            <div className="rounded-[12px] border-[1.5px] border-black flex items-center justify-between py-[16px] pr-[16px] pl-[12px]">
+                <SolarIconSet.Phone size={30} />
+                <Controller
+                    name="phoneNumber"
+                    control={props.control}
+                    render={() => (
+                        <input
+                            type="text"
+                            dir="ltr"
+                            placeholder="09111111111"
+                            className="bg-white border-none outline-none w-full"
+                        />
+                    )}
+                />
+            </div>
+        </div>
+    )
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,11 +3,22 @@ import { SigninMutation } from "../graphql/mutation/signup";
 
 
 import { useMutation } from "@apollo/client";
+import { useForm } from "react-hook-form";
+import FirstStep from "../components/layout/Login/FirstStep";
+import { useState } from "react";
 
 export default function Login() {
+  const {
+    register,
+    watch,
+    control,
+  } = useForm()
+  const [step, setStep] = useState<0 | 1>(0)
   const [signup, { data, loading, error }] = useMutation(SigninMutation);
-  signup({ variables: { phoneNumber: "09019605700" } })
-  const handleSignup = () => {}
+
+  const handleSignup = () => {
+    signup({ variables: { phoneNumber: watch("phoneNumber") } })
+  }
 
   return (
     <div className="text-black p-[24px] relative h-full" dir="rtl">
@@ -17,26 +28,13 @@ export default function Login() {
         {/* Progress bar */}
         <div className="flex justify-around gap-x-[1.81px] w-full">
           {/* Active Step */}
-          <div className="w-[143.09px] h-[3.62px] bg-[#ffcc4e] rounded-xl" />
+          <div className={`w-[143.09px] h-[3.62px] ${step === 0 ? "bg-[#ffcc4e]" : "bg-slate-100"} rounded-xl`} />
           {/* Inactive Step */}
-          <div className="w-[143.09px] h-[3.62px] bg-slate-100 rounded-xl" />
+          <div className={`w-[143.09px] h-[3.62px] ${step === 1 ? "bg-[#ffcc4e]" : "bg-slate-100"} rounded-xl`} />
         </div>
       </div>
       {/* Body */}
-      <div className="flex flex-col gap-y-[40px]">
-        <div className="flex flex-col gap-y-[16px]">
-          <h1 className="font-['IRANSansX'] text-[32px] font-bold">
-            شماره موبایل
-          </h1>
-          <p className="text-sm font-medium font-['IRANSansXFaNum'] leading-tight text-[#64748B]">
-            ما با اطمینان از واقعی بودن همه افرادی که در باکی هستند از کاربران خود محافظت می کنیم.
-          </p>
-        </div>
-        <div className="rounded-[12px] border-[1.5px] border-black flex items-center justify-between py-[16px] pr-[16px] pl-[12px]">
-          <SolarIconSet.Phone size={30} />
-          <input type="text" dir="ltr" placeholder="09111111111" className="bg-white border-none outline-none w-full" />
-        </div>
-      </div>
+      {step === 0 && <FirstStep control={control} />}
       {/* Footer */}
       <div className="absolute bottom-[24px] flex items-center justify-between gap-x-[16px]">
         <div className="flex items-center gap-x-[8px]">
@@ -46,7 +44,7 @@ export default function Login() {
             شماره موبایل شما در پروفایل شما نمایش داده نخواهد شد.
           </p>
         </div>
-        <button className="px-[20px] py-[16px] bg-slate-100 rounded-[12px] text-slate-400 font-bold font-['IRANSansXFaNum'] leading-none">
+        <button disabled={watch("phoneNumber")?.length !== 11} onClick={handleSignup} className={`px-[20px] py-[16px] ${watch("phoneNumber")?.length === 11 ? "bg-[#ffcc4e]" : "bg-slate-100"} rounded-[12px] text-slate-400 font-bold font-['IRANSansXFaNum'] leading-none`}>
           بعدی
         </button>
       </div>
