dd180e9e3ff0a0b2f8c6c2e76f814d326fdad4b7
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2024-10-20 22:08:51 +0330

    finish initial state of profile

---
+++ src/components/shared/Buttons/ArrowButton.tsx
@@ -0,0 +1,26 @@
+import * as SolarIconSet from 'solar-icon-set';
+
+export default function ArrowButton(props: {
+    text: any,
+    onClick?: () => void,
+    className?: string,
+    icon?: any,
+    arrowText?: string,
+}) {
+    return (
+        <div className={`flex items-center justify-between px-[16px] py-[12px] rounded-[12px] border border-slate-300 w-full ${props.className}`} onClick={props.onClick}>
+            <div className='flex items-center gap-x-2'>
+                {props.icon}
+                <p className='text-sm font-medium'>
+                    {props.text}
+                </p>
+            </div>
+            <div className='flex items-center gap-x-2'>
+                <p className='text-sm font-bold text-[#94A3B8]'>
+                    {props.arrowText}
+                </p>
+                <SolarIconSet.AltArrowLeft size={24} />
+            </div>
+        </div>
+    )
+}
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,10 +1,13 @@
 import * as SolarIconSet from 'solar-icon-set';
-import ComplateProfile from '../components/layout/Profile/ComplateProfile';
-import Authentication from '../components/layout/Profile/Authentication';
 import BakiBanner from "../assets/img/profile/BakiBanner.svg"
 import UploadPictures from '../components/shared/Inputs/UploadPictures';
 import TextInput from '../components/shared/Inputs/TextInput';
 import { useForm } from 'react-hook-form';
+import ArrowButton from '../components/shared/Buttons/ArrowButton';
+import { LiaSmokingSolid } from "react-icons/lia";
+import { PiGraphLight } from "react-icons/pi";
+import { TbZodiacAries } from "react-icons/tb";
+import { CiStar } from "react-icons/ci";
 
 export default function Profile() {
     const {
@@ -27,13 +30,13 @@ export default function Profile() {
                     <h2 className='text-[#64748B] text-sm font-semibold mr-3'>
                         تکمیل پروفایل:
                     </h2>
-                    <ComplateProfile />
+                    <ArrowButton text="65 درصد کامل شده" className='bg-brand-yellow' />
                 </div>
                 <div className='w-full'>
                     <h2 className='text-[#64748B] text-sm font-semibold mr-3'>
                         تایید هویت
                     </h2>
-                    <Authentication />
+                    <ArrowButton text="تایید شماره موبایل" />
                 </div>
                 <div className='py-8 w-full flex items-center'>
                     <img src={BakiBanner} alt="BakiBanner" />
@@ -46,17 +49,17 @@ export default function Profile() {
                     <UploadPictures />
                 </div>
                 {/* About Me */}
-                <div className='w-full flex flex-col items-center gap-y-3 min-h-fit max-w-fit'>
+                <div className='w-full flex flex-col items-center gap-y-3 min-h-fit'>
                     <h1 className='w-full text-end text-lg font-bold'>
                         درباره من
                     </h1>
-                    <div>
+                    <div className='w-full'>
                         <h2 className='text-[#64748B] text-sm font-semibold mr-3'>
                             نام
                         </h2>
                         <TextInput placeholder='نام' control={control} icon={<SolarIconSet.UserRounded size={30} />} name='name' value={watch("name")} />
                     </div>
-                    <div>
+                    <div className='w-full'>
                         <h2 className='text-[#64748B] text-sm font-semibold mr-3'>
                             نام کاربری
                         </h2>
@@ -67,6 +70,88 @@ export default function Profile() {
                             بیوگرافی
                         </h2>
                         <TextInput placeholder='از علایق، ویژگی‌ها و هرچی دوست دارین درباره خودتون بگید...' multiline={true} rows={3} control={control} name='biography' value={watch("biography")} />
+                    </div>
+                    <div className='w-full'>
+                        <h2 className='text-[#64748B] text-sm font-semibold mr-3'>
+                            محل زندگی
+                        </h2>
+                        <ArrowButton text="محل زندگی کنونی خود را وارد کنید" icon={<SolarIconSet.Home size={24} />} />
+                    </div>
+                </div>
+                {/* Basic Information */}
+                <div className='w-full flex flex-col items-center gap-y-3 min-h-fit'>
+                    <h1 className='w-full text-end text-lg font-bold'>
+                        اطلاعات اولیه
+                    </h1>
+                    <div className='w-full flex flex-col gap-y-2'>
+                        <ArrowButton text="جنسیت" arrowText='افزودن' className='w-full' icon={<SolarIconSet.UserId size={24} />} />
+                        <ArrowButton text="سال و ماه تولد" arrowText='افزودن' className='w-full' icon={<SolarIconSet.Calendar size={24} />} />
+                        <ArrowButton text="وضعیت تاهل" arrowText='افزودن' className='w-full' icon={<SolarIconSet.Heart size={24} />} />
+                        <ArrowButton text="سیگار" arrowText='افزودن' className='w-full' icon={<LiaSmokingSolid size={24} />} />
+                        <ArrowButton text="ورزش" arrowText='افزودن' className='w-full' icon={<SolarIconSet.DumbbellSmall size={24} />} />
+                        <ArrowButton text="میزان سحرخیزی" arrowText='افزودن' className='w-full' icon={<SolarIconSet.SunFog size={24} />} />
+                        <ArrowButton text="روحیه" arrowText='افزودن' className='w-full' icon={<SolarIconSet.MaskHapply size={24} />} />
+                        <div>
+                            <div className="flex items-center justify-between px-[16px] py-[12px] rounded-[12px] bg-[#F1F5F9] w-full">
+                                <div className='flex items-center gap-x-2'>
+                                    <PiGraphLight size={24} />
+                                    <p className='text-sm font-medium text-[#94A3B8]'>
+                                        نشان زودیاک
+                                    </p>
+                                </div>
+                                <div className='flex items-center gap-x-2'>
+                                    <p className='text-sm font-medium text-[#94A3B8]'>
+                                        قوچ
+                                    </p>
+                                    <TbZodiacAries size={24} className='text-[#94A3B8]' />
+                                </div>
+                            </div>
+                            <p className='text-[10px] pr-3 pt-1'>
+                                نشان زودیاک شما با توجه به ماه تولد شما تعیین شده است.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                 {/* More about Me */}
+                 <div className='w-full flex flex-col items-center gap-y-3 min-h-fit'>
+                    <h1 className='w-full text-end text-lg font-bold'>
+                        موارد بیشتر درباره من
+                    </h1>
+                    <div className='w-full'>
+                        <h2 className='text-[#64748B] text-sm font-semibold mr-3'>
+                            تخصص
+                        </h2>
+                        <ArrowButton text="اضافه کردن" className='w-full' icon={<SolarIconSet.SuitcaseTag size={24} />} />
+                    </div>
+                    <div className='w-full'>
+                        <h2 className='text-[#64748B] text-sm font-semibold mr-3'>
+                        زبان‌هایی که میدانم
+                        </h2>
+                        <ArrowButton text="اضافه کردن" className='w-full' icon={<SolarIconSet.Dialog size={24} />} />
+                    </div>
+                    <div className='w-full'>
+                        <h2 className='text-[#64748B] text-sm font-semibold mr-3'>
+                        مکان‌هایی که سفر کرده‌ام
+                        </h2>
+                        <ArrowButton text="اضافه کردن" className='w-full' icon={<SolarIconSet.Globus size={24} />} />
+                    </div>
+                    <div className='w-full'>
+                        <h2 className='text-[#64748B] text-sm font-semibold mr-3'>
+                        مکان‌هایی که زندگی کرده‌ام
+                        </h2>
+                        <ArrowButton text="اضافه کردن" className='w-full' icon={<SolarIconSet.Globus size={24} />} />
+                    </div>
+                    <div className='w-full'>
+                        <h2 className='text-[#64748B] text-sm font-semibold mr-3'>
+                        علایق شخصی من
+                        </h2>
+                        <ArrowButton text="اضافه کردن" className='w-full' icon={<CiStar size={24} />} />
+                    </div>
+                    <div className='w-full'>
+                        <h2 className='text-[#64748B] text-sm font-semibold mr-3'>
+                        علایق عمومی من در سفر
+                        </h2>
+                        <ArrowButton text="اضافه کردن" className='w-full' icon={<SolarIconSet.SuitcaseTag size={24} />} />
                     </div>
                 </div>
             </div>
