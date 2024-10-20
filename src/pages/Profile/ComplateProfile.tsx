bf5b0c34340b303558a32a9583461ebb0f2223a4
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2024-10-20 22:41:36 +0330

    finish design of complateProfile page and just need to link it to pages

--- src/pages/Profile/ComplateProfile.tsx
+++ src/pages/Profile/ComplateProfile.tsx
@@ -1,10 +1,26 @@
+import { CiStar } from 'react-icons/ci';
 import { Link } from 'react-router-dom';
 import * as SolarIconSet from 'solar-icon-set';
 
-const CustomeCard = () => {
+const CustomeCard = (props: {
+    icon: any,
+    status: boolean,
+    title: string,
+    description: string,
+}) => {
     return (
-        <div>
-            
+        <div className="h-[180px] w-full py-6 rounded-xl border border-slate-300 flex-col justify-start items-center gap-4 inline-flex">
+            <div className={`p-4 rounded-[40px] justify-center items-center gap-2 inline-flex ${props.status ? "bg-brand-green" : "bg-brand-yellow"}`}>
+                {props.icon}
+            </div>
+            <div className="flex-col justify-start items-center gap-2 flex">
+                <div className="text-center text-[#1a1d1e] text-base font-bold font-['IRANSansXFaNum'] leading-normal">
+                    {props.title}
+                </div>
+                <div className="text-center text-slate-400 text-sm font-medium font-['IRANSansXFaNum'] leading-tight">
+                    {props.description}
+                </div>
+            </div>
         </div>
     )
 }
@@ -27,8 +43,19 @@ export default function ComplateProfile() {
                         پروفایل خودتو تکمیل کن!
                     </h1>
                     <p className='text-sm text-[#64748B] font-medium'>
-                    هر چقدر در تکمیل اطلاعات صادقتر باشید افرادی با شرایط مشابه بیشتری به شما معرفی خواهد شد و امکان مسافرت مناسب‌تر برا شما فراهم می‌شود.
+                        هر چقدر در تکمیل اطلاعات صادقتر باشید افرادی با شرایط مشابه بیشتری به شما معرفی خواهد شد و امکان مسافرت مناسب‌تر برا شما فراهم می‌شود.
                     </p>
+                </div>
+                {/* Cards */}
+                <div className='w-full grid grid-cols-2 gap-5 py-4 px-2'>
+                    <CustomeCard icon={<SolarIconSet.PenNewSquare size={32} />} title='بیوگرافی' description='نوشته نشده' status={false} />
+                    <CustomeCard icon={<SolarIconSet.VerifiedCheck size={32} />} title='تایید هویت' description='انجام نشده' status={false} />
+                    <CustomeCard icon={<SolarIconSet.Gallery size={32} />} title='تصاویر' description='0 از 4 آپلود شده' status={false} />
+                    <CustomeCard icon={<SolarIconSet.InfoCircle size={32} />} title='اطلاعات اولیه' description='0 از 7 اضافه شده' status={false} />
+                    <CustomeCard icon={<SolarIconSet.StickerSmileSquare size={32} />} title='آواتار' description='انتخاب نشده' status={false} />
+                    <CustomeCard icon={<SolarIconSet.Microphone size={32} />} title='پیام خوش‌آمد گویی' description='ضبط نشده' status={false} />
+                    <CustomeCard icon={<CiStar size={32} />} title='علایق شخصی' description='0 از 5 اضافه شده' status={false} />
+                    <CustomeCard icon={<SolarIconSet.SuitcaseTag size={32} />} title='علایق عمومی در سفر' description='0 از 5 اضافه شده' status={false} />
                 </div>
             </div>
         </div>
