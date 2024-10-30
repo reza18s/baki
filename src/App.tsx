dd0a86d5c7ec5c411d3add1978ebbe25b5f692fa
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2024-10-30 22:25:06 +0330

    still has routing bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,51 @@
-import { Route } from 'react-router-dom';
-import {
-  IonApp,
-  IonIcon,
-  IonLabel,
+import { Route, Routes } from 'react-router-dom';
+import { 
+  IonApp, 
   IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  setupIonicReact
+  setupIonicReact 
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Routes from './Routes/Routes';
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-import "./assets/main.css"
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
-
-/* Theme variables */
-import './theme/variables.css';
+/* Import your components */
 import Index from './pages';
 import Signup from './pages/signup';
 import Profile from './pages/Profile/profile';
 import ComplateProfile from './pages/Profile/ComplateProfile';
 import IdentityVerification from './components/layout/Profile/IdentityVerification';
 
+/* Import your CSS files */
+import '@ionic/react/css/core.css';
+import "./assets/main.css"
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+import '@ionic/react/css/palettes/dark.system.css';
+import './theme/variables.css';
+
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/" element={<Index />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/profile">
-          <Route path="/complate_profile" element={<ComplateProfile />} />
-          <Route path="/complate_profile">
-            <Route path="/identity_verification" element={<IdentityVerification />} />
-          </Route>
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile/complate_profile" element={<ComplateProfile />} />
+            <Route path="/profile/complate_profile/identity_verification" element={<IdentityVerification />} />
+          </Routes>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
