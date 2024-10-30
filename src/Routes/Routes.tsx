8ae3581450743fa434e44eb63fcdb0bbe8c3b2c0
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2024-10-30 22:49:07 +0330

    complate structure of routing

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,4 +1,3 @@
-import { Route } from 'react-router-dom';
 import {
   IonRouterOutlet,
 } from '@ionic/react';
@@ -7,18 +6,37 @@ import Index from '../pages';
 import Profile from '../pages/Profile/profile';
 import Signup from '../pages/signup';
 import ComplateProfile from '../pages/Profile/ComplateProfile';
+import { Route, Switch } from 'react-router-dom';
+import ProfileLayout from '../components/layout/Profile/Layout';
 
 export default function Routes() {
-  return(
+  return (
     <IonReactRouter>
-    <IonRouterOutlet>
-      <Route path="/" component={Index} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/profile">
-      <Route path="/complate_profile" component={ComplateProfile} />
-      </Route>
-    </IonRouterOutlet>
-  </IonReactRouter>
-  )
+      <IonRouterOutlet>
+        <Switch>
+          <Route exact path="/" component={Index} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/profile/complate_profile" component={ComplateProfile} />
+          
+          {/* Profile Routes with Layout */}
+          {/* <Route 
+            path="/profile"
+            render={({ match }) => (
+              <ProfileLayout>
+                <Switch>
+                  <Route exact path={match.path} component={Profile} />
+                  <Route 
+                    exact 
+                    path={`${match.path}/complate_profile`} 
+                    component={ComplateProfile} 
+                  />
+                </Switch>
+              </ProfileLayout>
+            )}
+          /> */}
+        </Switch>
+      </IonRouterOutlet>
+    </IonReactRouter>
+  );
 }
---
+++ src/components/layout/Profile/Layout.tsx
@@ -0,0 +1,10 @@
+export default function ProfileLayout({ children }: {
+    children: any;
+}) {
+    return (
+        <div>
+            <div>asdf</div>
+            {children}
+        </div>
+    )
+}
