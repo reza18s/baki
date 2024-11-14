80a7c0b427bdd4ee1b4be56a20a6e1c15e6d77ad
reza18s <swary2021@gmail.com>
2024-11-14 17:19:01 +0330

    add the paths

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,19 +1,21 @@
-import { IonRouterOutlet } from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
-import Index from "../pages";
-import Profile from "../pages/Profile/profile";
-import Signup from "../pages/signup";
-import ComplateProfile from "../pages/Profile/ComplateProfile";
-import { Route, Switch } from "react-router-dom";
-import IdentityVerification from "../components/layout/Profile/IdentityVerification";
-import ComplatePictures from "../components/layout/Profile/ComplatePictures";
-import ComplateGeneralInterests from "../components/layout/Profile/ComplateGeneralInterests";
-import ComplatePersonalInterests from "../components/layout/Profile/ComplatePersonalInterests";
-import ComplateResidenceCity from "../components/layout/Profile/ComplateResidenceCity";
-import ComplateSpecialty from "../components/layout/Profile/ComplateSpecialty";
-import BasicInformations from "../components/layout/Profile/BasicInformations/BasicInformations";
-import Explore from "../pages/Explore.tsx/Explore";
-import MainLayout from "./MainLayout";
+import { IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import Index from '../pages';
+import Profile from '../pages/Profile/profile';
+import Signup from '../pages/signup';
+import ComplateProfile from '../pages/Profile/ComplateProfile';
+import { Route, Switch } from 'react-router-dom';
+import IdentityVerification from '../components/layout/Profile/IdentityVerification';
+import ComplatePictures from '../components/layout/Profile/ComplatePictures';
+import ComplateGeneralInterests from '../components/layout/Profile/ComplateGeneralInterests';
+import ComplatePersonalInterests from '../components/layout/Profile/ComplatePersonalInterests';
+import ComplateResidenceCity from '../components/layout/Profile/ComplateResidenceCity';
+import ComplateSpecialty from '../components/layout/Profile/ComplateSpecialty';
+import BasicInformations from '../components/layout/Profile/BasicInformations/BasicInformations';
+import Explore from '../pages/Explore/index';
+import MainLayout from './MainLayout';
+import { Filter } from '@/pages/Explore/Filter';
+import { paths } from './paths';
 
 export default function Routes() {
   return (
@@ -63,10 +65,11 @@ export default function Routes() {
             path="/profile/complate_profile/complate_basicinformations"
             component={BasicInformations}
           />
+          <Route exact path={paths.explore.filter} component={Filter} />
 
           {/* Explore Routes with Layout */}
           <Route
-            path="/explore"
+            path={paths.explore.path}
             render={({ match }) => (
               <MainLayout>
                 <Switch>
