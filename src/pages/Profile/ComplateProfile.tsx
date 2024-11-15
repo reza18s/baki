114613828caf4cf68824b793b7870525677d0edc
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2024-11-15 11:37:41 +0330

    add update status and description of cards to complateProfile page

--- src/pages/Profile/ComplateProfile.tsx
+++ src/pages/Profile/ComplateProfile.tsx
@@ -1,3 +1,4 @@
+import { useLocalStore } from "@/store/useLocalStore";
 import { CiStar } from "react-icons/ci";
 import { Link } from "react-router-dom";
 import * as SolarIconSet from "solar-icon-set";
@@ -32,6 +33,82 @@ const CustomeCard = (props: {
 };
 
 export default function ComplateProfile() {
+  const userInfo = useLocalStore((store) => store.userInfo);
+
+  const cardsData = [
+    {
+      icon: <SolarIconSet.PenNewSquare size={32} />,
+      title: "بیوگرافی",
+      description: userInfo.bio ? "نوشته شده" : "نوشته نشده",
+      url: "/profile#biography",
+      status: !!userInfo.bio,
+    },
+    {
+      icon: <SolarIconSet.VerifiedCheck size={32} />,
+      title: "تایید هویت",
+      description: userInfo.gender ? "انجام شده" : "انجام نشده",
+      url: "/profile/complate_profile/identify_verification",
+      status: !!userInfo.gender,
+    },
+    {
+      icon: <SolarIconSet.Gallery size={32} />,
+      title: "تصاویر",
+      description: `${userInfo.pictures.length} از 4 آپلود شده`,
+      url: "/profile/complate_profile/complate_pictures",
+      status: userInfo.pictures.length === 4,
+    },
+    {
+      icon: <SolarIconSet.InfoCircle size={32} />,
+      title: "اطلاعات اولیه",
+      description: userInfo.name ? "1 از 7 اضافه شده" : "0 از 7 اضافه شده", // Example logic
+      url: "/profile/complate_profile/complate_basicinformations",
+      status: !!userInfo.name,
+    },
+    {
+      icon: <SolarIconSet.StickerSmileSquare size={32} />,
+      title: "آواتار",
+      description: userInfo.username ? "انتخاب شده" : "انتخاب نشده",
+      url: "/profile/complate_profile/",
+      status: !!userInfo.username,
+    },
+    {
+      icon: <SolarIconSet.Microphone size={32} />,
+      title: "پیام خوش‌آمد گویی",
+      description: "ضبط نشده", // Adjust logic as needed
+      url: "/profile/complate_profile/",
+      status: false, // Adjust logic as needed
+    },
+    {
+      icon: <CiStar size={32} />,
+      title: "علایق شخصی",
+      description: `${userInfo.personalInterests.length} از 5 اضافه شده`,
+      url: "/profile/complate_profile/complate_personalInterests",
+      status: userInfo.personalInterests.length === 5,
+    },
+    {
+      icon: <SolarIconSet.SuitcaseTag size={32} />,
+      title: "علایق عمومی در سفر",
+      description: `${userInfo.travelsInterests.length} از 5 اضافه شده`,
+      url: "/profile/complate_profile/complate_generalinterests",
+      status: userInfo.travelsInterests.length === 5,
+    },
+    {
+      icon: <SolarIconSet.Accessibility size={32} />,
+      title: "تخصص",
+      description: userInfo.specialty.length ? "انتخاب شده" : "انتخاب نشده",
+      url: "/profile/complate_profile/complate_specialty",
+      status: !!userInfo.specialty.length,
+    },
+    {
+      icon: <SolarIconSet.Home size={32} />,
+      title: "محل زندگی",
+      description: userInfo.residenceCity ? "انتخاب شده" : "انتخاب نشده",
+      url: "/profile/complate_profile/complate_residencecity",
+      status: !!userInfo.residenceCity,
+    },
+  ];
+
+
   return (
     <div className="w-full flex flex-col items-center gap-y-3 h-full pb-16 overflow-y-auto">
       {/* Head */}
@@ -54,76 +131,16 @@ export default function ComplateProfile() {
         </div>
         {/* Cards */}
         <div className="w-full grid grid-cols-2 gap-5 py-4 px-2">
-          <CustomeCard
-            icon={<SolarIconSet.PenNewSquare size={32} />}
-            title="بیوگرافی"
-            description="نوشته نشده"
-            url="/profile#biography"
-            status={false}
-          />
-          <CustomeCard
-            icon={<SolarIconSet.VerifiedCheck size={32} />}
-            title="تایید هویت"
-            description="انجام نشده"
-            url="/profile/complate_profile/identify_verification"
-            status={false}
-          />
-          <CustomeCard
-            icon={<SolarIconSet.Gallery size={32} />}
-            title="تصاویر"
-            description="0 از 4 آپلود شده"
-            url="/profile/complate_profile/complate_pictures"
-            status={false}
-          />
-          <CustomeCard
-            icon={<SolarIconSet.InfoCircle size={32} />}
-            title="اطلاعات اولیه"
-            description="0 از 7 اضافه شده"
-            url="/profile/complate_profile/complate_basicinformations"
-            status={false}
-          />
-          <CustomeCard
-            icon={<SolarIconSet.StickerSmileSquare size={32} />}
-            title="آواتار"
-            description="انتخاب نشده"
-            url="/profile/complate_profile/"
-            status={false}
-          />
-          <CustomeCard
-            icon={<SolarIconSet.Microphone size={32} />}
-            title="پیام خوش‌آمد گویی"
-            description="ضبط نشده"
-            url="/profile/complate_profile/"
-            status={false}
-          />
-          <CustomeCard
-            icon={<CiStar size={32} />}
-            title="علایق شخصی"
-            description="0 از 5 اضافه شده"
-            url="/profile/complate_profile/complate_personalInterests"
-            status={false}
-          />
-          <CustomeCard
-            icon={<SolarIconSet.SuitcaseTag size={32} />}
-            title="علایق عمومی در سفر"
-            description="0 از 5 اضافه شده"
-            url="/profile/complate_profile/complate_generalinterests"
-            status={false}
-          />
-          <CustomeCard
-            icon={<SolarIconSet.Accessibility size={32} />}
-            title="تخصص"
-            description="انتخاب نشده"
-            url="/profile/complate_profile/complate_specialty"
-            status={false}
-          />
-          <CustomeCard
-            icon={<SolarIconSet.Home size={32} />}
-            title="محل زندگی"
-            description="انتخاب نشده"
-            url="/profile/complate_profile/complate_residencecity"
-            status={false}
-          />
+        {cardsData.map((card, index) => (
+            <CustomeCard
+              key={index}
+              icon={card.icon}
+              title={card.title}
+              description={card.description}
+              url={card.url}
+              status={card.status}
+            />
+          ))}
         </div>
       </div>
     </div>
