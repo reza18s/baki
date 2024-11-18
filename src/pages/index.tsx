a82b4ee2f4f7ff1a91078c307ac108ab2297a8f8
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2024-11-19 00:38:26 +0330

    add gradient to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,17 +9,17 @@ export default function Index() {
 
   return (
     <div
-      className="h-screen bg-cover bg-center"
+      className="h-screen bg-cover bg-center relative"
       style={{ backgroundImage: `url(${BackgroundImage})` }}
     >
-      <div className="flex h-[90vh] w-full flex-col items-center justify-between px-[24px] pb-[6px] pt-[52px] text-white">
+      <div className="flex h-[90vh] w-full flex-col items-center justify-between px-[24px] pb-[50px] pt-[65px] text-white">
         <div className="flex max-h-fit flex-col items-center justify-between gap-y-[8px]">
           <img src={BakiLogo} alt="BakiLogo" />
           <h2 className="text-sm font-bold leading-tight text-white">
             راه حلی برای سفر های از دست رفته
           </h2>
         </div>
-        <div className="flex w-full flex-col items-center justify-between gap-y-[16px]">
+        <div className="flex w-full flex-col items-center justify-between gap-y-[16px] z-10">
           <div className="flex w-full flex-col items-center justify-between gap-y-[8px] text-center">
             <Link
               to="/signup"
@@ -52,6 +52,8 @@ export default function Index() {
           </Modal>
         )}
       </div>
+      {/* gradient div */}
+      <div className='absolute w-full h-[40vh] bottom-0 bg-gradient-to-t from-[#9a9692]' />
     </div>
   );
 }
