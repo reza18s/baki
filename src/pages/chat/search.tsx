b26a34b8930efeedd695bdb6764c9b89ab9ceae6
reza18s <swary2021@gmail.com>
2024-12-15 22:28:08 +0330

    add the search

--- src/pages/chat/search.tsx
+++ src/pages/chat/search.tsx
@@ -10,10 +10,12 @@ import {
   User,
 } from '@/graphql/generated/graphql.codegen';
 import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 
 export const Search = () => {
   const { data, loading: contactLoading } = useGetChatsQuery();
-  const [search, setSearch] = useState<string>();
+  const [search, setSearch] = useState<string>('');
+  const hs = useHistory();
   const { data: me } = useGetMeQuery({
     onError(err) {
       if (err.message === 'Failed to fetch') {
@@ -32,7 +34,11 @@ export const Search = () => {
             value={search}
             onChange={(e) => setSearch(e.target.value)}
           ></Input>
-          <Button variant="text" className="text-brand-red">
+          <Button
+            variant="text"
+            className="text-brand-red"
+            onClick={() => hs.goBack()}
+          >
             بازگشت
           </Button>
         </div>
@@ -41,16 +47,20 @@ export const Search = () => {
       className=""
       isLoading={contactLoading}
     >
-      {(data?.getChats?.length || 0) > 0 ? (
+      {(data?.getChats?.filter((el) =>
+        el.participants?.[0]?.name?.includes(search),
+      ).length || 0) > 0 && search.length !== 0 ? (
         <div className="flex flex-col rounded-xl border border-gray-300 bg-white px-3 py-2">
-          {data?.getChats?.map((chat) => (
-            <Contact
-              key={chat?.id}
-              className="flex w-full items-center gap-2 transition-all duration-300 ease-in-out active:bg-gray-100"
-              chat={chat as Chat}
-              me={me?.getMe as User}
-            ></Contact>
-          ))}
+          {data?.getChats
+            ?.filter((el) => el.participants?.[0]?.name?.includes(search))
+            .map((chat) => (
+              <Contact
+                key={chat?.id}
+                className="flex w-full items-center gap-2 transition-all duration-300 ease-in-out active:bg-gray-100"
+                chat={chat as Chat}
+                me={me?.getMe as User}
+              ></Contact>
+            ))}
         </div>
       ) : (
         <div className="flex h-full w-full flex-col items-center justify-center gap-4"></div>
